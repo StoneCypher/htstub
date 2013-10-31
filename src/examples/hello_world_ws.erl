05bdbd1fc9a592307207d5df54fba2e4c4f030ab
John Haugeland <stonecypher@gmail.com>
2013-10-31 00:21:13 -0700

    simplified the docs

--- src/examples/hello_world_ws.erl
+++ src/examples/hello_world_ws.erl
@@ -17,19 +17,17 @@
 %%
 %%  @doc Returns a "hello, world" webpage.
 %%
-%%  This is an intentionally trivial webserver driver.  This is essentially the simplest that it gets:
-%%  a rendering function that completely ignores the request and returns a trivial fixed string.  When
-%%  passed a string, htstub will assume that the request is `200 OK` and return it as such.  
+%%  This is an intentionally trivial webserver driver.  This completely ignores the request and
+%%  returns a string constant.  (htstub interprets string constants as `200, ok`.)
 %%
-%%  This is meant as someone's first steps with htstub, so usage is kept simple.  ```1> htstub:serve( fun hello_world_ws:page/1 ).
+%%  This is meant as first steps with htstub; usage is kept simple.  
+%%
+%%  ```1> htstub:serve( fun hello_world_ws:page/1 ).
 %%  <0.45.0>'''
 %%
 %%  That's it.  There's now <a href="http://localhost/">a webserver running</a>, assuming you have 
-%%  rights to port 80 and it isn't already in use.  If 80 is unavailable, try this instead: ```1> htstub:serve( fun hello_world_ws:page/1, 9717 ).
-%%  <0.45.0>'''
-%%
-%%  Now you should have <a href="http://localhost:9717/">a webserver on 9717</a> instead.  (If 9717 is 
-%%  also in use, you'll have to pick a number of your own.)
+%%  rights to port 80.  (If not, `1> htstub:serve( fun hello_world_ws:page/1, 9717 )` or whatever.)
+
 
 page( _ ) ->
 
